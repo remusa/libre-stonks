2ae66f23a02973ca87aa241bbc9d205acddec073
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-01-18 19:20:44 -0600

    chore: fix sveltekit port

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,30 @@
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-static' // This was changed from adapter-auto
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-export default {
+/** @type {import('@sveltejs/kit').Config}*/
+const config = {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: [vitePreprocess({})],
-};
+
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true,
+    }),
+
+    alias: {
+      // '@/*': './src/lib/*',
+    },
+  },
+}
+
+export default config
