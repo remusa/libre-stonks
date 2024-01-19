553ce35848de11f11549012709e329c47be6bdd0
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-01-19 00:28:10 -0600

    chore: add tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,133 @@
 <script lang="ts">
-  import Greet from './lib/Greet.svelte'
+  import Search from '$lib/components/Search.svelte'
+  import { Button } from '$lib/components/ui/button'
+  import * as Card from '$lib/components/ui/card'
+  import { Input } from '$lib/components/ui/input'
+  import { Label } from '$lib/components/ui/label'
+  import * as Tabs from '$lib/components/ui/tabs'
+  import clsx from 'clsx'
+  import dayjs from 'dayjs'
+
+  let stocks = [
+    {
+      name: 'Google',
+      ticker: 'GOOG',
+      price: 17.55,
+      date: dayjs(),
+    },
+    {
+      name: 'Apple',
+      ticker: 'APPL',
+      price: -69.99,
+      date: dayjs(),
+    },
+  ]
+  let search = ''
+  console.log(`🚀 ~ search:`, search)
 </script>
 
-<main class="container">
-  <h1>Welcome to Tauri!</h1>
+<main class="flex flex-col items-center justify-start w-screen h-screen p-4 rounded-3xl shadow-3xl m-0">
+  <header>
+    <h1 class="logo vite">Stock tracker</h1>
+  </header>
 
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <Tabs.Root value="home" class="">
+    <Tabs.List class="grid w-full grid-cols-2">
+      <Tabs.Trigger value="home">Home</Tabs.Trigger>
+      <Tabs.Trigger value="settings">Settings</Tabs.Trigger>
+    </Tabs.List>
+
+    <Tabs.Content value="home">
+      <Card.Root>
+        <Card.Header>
+          <Card.Title>Home</Card.Title>
+          <Card.Description>Add stonks here. Click save when you're done.</Card.Description>
+        </Card.Header>
+        <Card.Content class="space-y-2">
+
+
+
+          
+        </Card.Content>
+        <Card.Footer>
+          <Button>Search</Button>
+        </Card.Footer>
+      </Card.Root>
+    </Tabs.Content>
+
+    <Tabs.Content value="home">
+      <Card.Root>
+        <Card.Header>
+          <Card.Title>Home</Card.Title>
+          <Card.Description>Add stonks here. Click save when you're done.</Card.Description>
+        </Card.Header>
+        <Card.Content class="space-y-2">
+          <div class="space-y-1">
+            <Label for="search">Search</Label>
+            <Input id="search" {search} />
+          </div>
+        </Card.Content>
+        <Card.Footer>
+          <Button>Search</Button>
+        </Card.Footer>
+      </Card.Root>
+    </Tabs.Content>
+  </Tabs.Root>
+
+  <div class="flex flex-col justify-start items-center w-screen">
+    <!-- <Greet /> -->
+    <Search />
   </div>
 
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
-
-  <div class="row">
-    <Greet />
+  <div class="w-full flex flex-col gap-2 items-center">
+    <h2 class="bold">Stocks</h2>
+    <ul class="flex w-full flex-col gap-2 divide-y-2 divide-grey-900 divide-dashed">
+      {#each stocks as item, i (item.ticker)}
+        {@const noChange = item.price === 0}
+        {@const increment = item.price > 0}
+        {@const decrement = item.price < 0}
+        <li class="grid-cols-12 w-full grid pt-2">
+          <span class="col-span-7">
+            <div>
+              <span class="p-1 rounded bold text-blue-900 bg-blue-200 w-16">
+                {item.ticker}
+              </span>
+            </div>
+            <div>
+              <span class="bold">
+                {item.name}
+              </span>
+            </div>
+          </span>
+          <span class="col-span-5 grid grid-cols-2 gap-3 justify-center items-center">
+            <span class="text-right">{item.price}</span>
+            <span
+              class={clsx(
+                'rounded p-1 text-white flex justify-between gap-0 bold',
+                increment && 'bg-green-500',
+                decrement && 'bg-red-500',
+                noChange && 'bg-grey-500',
+              )}
+            >
+              <span>
+                {#if increment}
+                  +
+                {:else if decrement}
+                  -
+                {:else}
+                  ' '
+                {/if}
+              </span>
+              <span>
+                {Math.abs(item.price)}
+              </span>
+              <span>%</span>
+            </span>
+          </span>
+        </li>
+      {/each}
+    </ul>
   </div>
-
-
 </main>
 
 <style>
---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Input } from '$lib/components/ui/input'
+  import { Label } from '$lib/components/ui/label'
+
+</script>
+
+<div>
+  <div class="flex flex-col w-full max-w-sm gap-1.5">
+    <Label for="search">Search</Label>
+    <Input type="text" id="search" placeholder="Add ticker..." />
+  </div>
+</div>
