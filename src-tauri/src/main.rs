e5431408f284373ebed808c8c738aa1d18b4ec9b
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-01-19 00:12:13 -0600

    chore: add window transparency & shadows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use window_shadows::set_shadow;
+use window_vibrancy::{apply_acrylic, apply_blur, apply_vibrancy, NSVisualEffectMaterial};
+
 fn main() {
-    libre_stocks_lib::run()
+    libre_stocks_lib::run();
+
+    tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+            #[cfg(target_os = "windows")]
+            // apply_blur(&window, Some((18, 18, 18, 125)))
+            apply_acrylic(&window, Some((0, 0, 0, 10)))
+                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+            #[cfg(any(windows, target_os = "macos"))]
+            set_shadow(&window, true).unwrap();
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import App from "./App.svelte";
 import "./app.pcss";
 import "./styles.css";
-import App from "./App.svelte";
 
 const app = new App({
 	target: document.getElementById("app"),
