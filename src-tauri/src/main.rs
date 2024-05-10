722d0699cf5721166629f26f6ca60f4884ccfcdb
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-05-10 17:39:13 -0600

    build(migrate): migrate cli

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
 
     tauri::Builder::default()
         .setup(|app| {
-            let window = app.get_window("main").unwrap();
+            let window = app.get_webview_window("main").unwrap();
 
             #[cfg(target_os = "macos")]
             apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
